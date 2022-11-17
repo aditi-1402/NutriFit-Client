1ff6d16a20e92b6c50c19c3b98a7d67729e7bff0
aditi-1402 <74909479+aditi-1402@users.noreply.github.com>
2022-11-17 22:49:04 +0530

    Added files
    Frontend files

---
+++ website/auth.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return render_template("home.html")
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        dob = request.form.get('dob')
+        sex = request.form.get('sex')
+        height = request.form.get('height')
+        weight = request.form.get('weight')
+        password1 = request.form.get('password')
+        password2 = request.form.get('password2')
+        print(password1, password2)
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif len(last_name) < 2:
+            flash('Last name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            # Add user to database
+            flash('Account created!', category='success')
+    return render_template("sign_up.html")
---
+++ website/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
+@views.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+@views.route('/add_meal')
+def add_food():
+    return render_template("add_food.html")
+    
+@views.route('/reports')
+def reports():
+    return render_template("reports.html")
